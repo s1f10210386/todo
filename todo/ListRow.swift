88ab1fcf45cb12bdb1a7a548810e3e0d78c8aa2f
hotaka <s1f102103864@iniad.org>
2024-02-02 02:38:32 +0900

    feat : add List

---
+++ todo/ListRow.swift
@@ -0,0 +1,18 @@
+//
+//  ListRow.swift
+//  todo
+//
+//  Created by 金澤帆高 on 2024/02/02.
+//
+
+import SwiftUI
+
+struct ListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRow()
+}
