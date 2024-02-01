952d169292ca3621dd1d4ae298b7f3bed72cb519
hotaka <s1f102103864@iniad.org>
2024-02-02 02:06:50 +0900

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by 金澤帆高 on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
